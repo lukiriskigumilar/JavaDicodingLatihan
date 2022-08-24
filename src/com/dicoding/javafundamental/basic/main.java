3af34215ffe4aa102a123b7d4e529fb98842c455
luki riski <riskidevelopment@gmail.com>
2022-08-24 14:29:21 +0700

    komit yang kedua

--- src/com/dicoding/javafundamental/basic/main.java
+++ src/com/dicoding/javafundamental/basic/main.java
@@ -2,6 +2,6 @@ package com.dicoding.javafundamental.basic;
 
 public class main {
     public static void main (String[] args){
-        System.out.println("Hello world ");
+        System.out.println("saya coba komit yang kedua");
     }
 }
